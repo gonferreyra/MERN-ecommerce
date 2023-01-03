1119ee1d3fbb3f346313ca42e4a1b4cb9477c1b8
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-01-03 11:24:42 -0300

    add productReducer

--- Client/src/redux/rootReducer.js
+++ Client/src/redux/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./Auth/authReducer";
+import productReducer from "./Products/products-reducer";
 import shopReducer from "./Shopping/shopping-reducer"
 import { uiReducer } from "./UiReducer/ui-reducer";
 
@@ -7,6 +8,7 @@ const rootReducer = combineReducers({
     shop: shopReducer,
     auth: authReducer,
     ui: uiReducer,
+    products: productReducer
 });
 
 export default rootReducer
