bdc3e678dcf845b96673c6336cd6c43705bdc0e9
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-01-08 10:53:57 -0300

    redux - update product

--- Client/src/redux/Products/products-actions.js
+++ Client/src/redux/Products/products-actions.js
@@ -1,37 +1,75 @@
 import axiosClient from "../../helpers/axios.js";
+import { fetchWithoutToken, fetchWithToken } from "../../helpers/fetch.js";
 import { types } from "./products-types.js";
 
 // Function that download products from data base
 export const getProducts = () => {
-    return async (dispatch) => {
-        dispatch(downloadProducts())
-        try {
-            const resp = await axiosClient.get('/api/products');
-            dispatch(downloadProductsSuccess(resp.data))
-        } catch (error) {
-            console.log(error)
-            dispatch(downloadProductsError())
-        }
+  return async (dispatch) => {
+    dispatch(downloadProducts());
+    try {
+      const resp = await fetchWithoutToken("products");
+      const data = await resp.json();
+      dispatch(downloadProductsSuccess(data));
+    } catch (error) {
+      console.log(error);
+      dispatch(downloadProductsError());
     }
-}
+  };
+};
+
+export const updateProductAction = (updatedProduct) => {
+  return async (dispatch) => {
+    dispatch(updateProduct());
+    try {
+      await fetchWithToken(
+        `products/${updatedProduct._id}`,
+        updatedProduct,
+        "PUT"
+      );
+      dispatch(productUpdatedSuccess(updatedProduct));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 
 const downloadProducts = () => {
-    return {
-        type: types.START_DOWNLOADING_PRODUCTS,
-        payload: true
-    }
-}
+  return {
+    type: types.START_DOWNLOADING_PRODUCTS,
+    payload: true,
+  };
+};
 
 const downloadProductsSuccess = (products) => {
-    return {
-        type: types.PRODUCT_DOWNLOAD_SUCCESSFULL,
-        payload: products
-    }
+  return {
+    type: types.PRODUCT_DOWNLOAD_SUCCESSFULL,
+    payload: products,
+  };
 };
 
 const downloadProductsError = () => {
-    return {
-        type: types.PRODUCT_DOWNLOAD_ERROR,
-        payload: true
-    }
+  return {
+    type: types.PRODUCT_DOWNLOAD_ERROR,
+    payload: true,
+  };
+};
+
+export const loadCurrentItem = (item) => {
+  return {
+    type: types.PRODUCT_LOAD_CURRENT_ITEM,
+    payload: item,
+  };
+};
+
+const updateProduct = (id, product) => {
+  return {
+    type: types.PRODUCT_UPDATE,
+  };
+};
+
+const productUpdatedSuccess = (updatedProduct) => {
+  return {
+    type: types.PRODUCT_UPDATED_SUCCESSFULLY,
+    payload: updatedProduct,
+  };
 };
--- Client/src/redux/Products/products-reducer.js
+++ Client/src/redux/Products/products-reducer.js
@@ -1,34 +1,51 @@
-import { types } from './products-types.js'
+import { types } from "./products-types.js";
 
 const initialState = {
-    products: [],
-    error: null,
-    loading: false
-}
+  products: [],
+  error: null,
+  loading: false,
+  currentItem: null,
+};
 
 const productReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case types.START_DOWNLOADING_PRODUCTS:
-            return {
-                ...state,
-                loading: action.payload
-            }
-        case types.PRODUCT_DOWNLOAD_SUCCESSFULL:
-            return {
-                ...state,
-                loading: false,
-                error: null,
-                products: action.payload
-            }
-        case types.PRODUCT_DOWNLOAD_ERROR:
-            return {
-                ...state,
-                loading: false,
-                error: action.payload
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case types.START_DOWNLOADING_PRODUCTS:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case types.PRODUCT_DOWNLOAD_SUCCESSFULL:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        products: action.payload,
+      };
+    case types.PRODUCT_DOWNLOAD_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case types.PRODUCT_LOAD_CURRENT_ITEM:
+      return {
+        ...state,
+        currentItem: action.payload,
+      };
+    case types.PRODUCT_UPDATED_SUCCESSFULLY:
+      return {
+        ...state,
+        currentItem: null,
+        products: state.products.map((product) =>
+          // Me reemplaza todo y pierdo los datos que no ingreso como imgUrl... ver como hacer para que reemplace solo los modificados
+          product._id === action.payload._id
+            ? (product = action.payload)
+            : product
+        ),
+      };
+    default:
+      return state;
+  }
 };
 
 export default productReducer;
