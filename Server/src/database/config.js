5b44154b276073207ee4e11f21ec4dc231d61fa4
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-01-13 11:08:45 -0300

    code format and deleted Role model

--- Server/src/database/config.js
+++ Server/src/database/config.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const dbConection = async () => {
-    try {
-        mongoose.set("strictQuery", false);
-        await mongoose.connect(process.env.DB_CNN);
-        console.log("Database online")
-
-    } catch (error) {
-        console.log(error)
-        throw new Error("Error initializing Database")
-    }
+  try {
+    // warning on console from mongoose, use strickQuery false to remove it
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.DB_CNN);
+    console.log("Database online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error initializing Database");
+  }
 };
 
 export default dbConection;
--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -1,49 +1,50 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     img: {
-        type: String,
+      type: String,
     },
     role: {
-        type: String,
-        required: true,
-        emun: ['ADMIN_ROLE', 'USER_ROLE'],
-        default: 'USER_ROLE'
+      type: String,
+      required: true,
+      emun: ["ADMIN_ROLE", "USER_ROLE"],
+      default: "USER_ROLE",
     },
     google: {
-        type: Boolean,
-        default: false
-    }
-},
-    {
-        timestamps: true,
-        versionKey: false
-    }
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 // Encrypt password function
 userSchema.statics.encryptPassword = async (password) => {
-    const salt = await bcrypt.genSalt();
-    return bcrypt.hash(password, salt)
+  const salt = await bcrypt.genSalt();
+  return bcrypt.hash(password, salt);
 };
 
 // Compare password in login function
 userSchema.statics.comparePassword = async (password, receivedPassword) => {
-    return bcrypt.compare(password, receivedPassword)
+  return bcrypt.compare(password, receivedPassword);
 };
 
 export default mongoose.model("User", userSchema);
