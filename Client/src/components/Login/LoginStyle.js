e28f032b1815a7b50023a498bfe393e5d460dd57
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-01-22 20:32:41 -0300

    height changes to components

--- Client/src/components/Login/LoginStyle.js
+++ Client/src/components/Login/LoginStyle.js
@@ -1,155 +1,147 @@
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const Main = styled.main`
-    padding: 3rem 0;
-    background-color: #6e85b7;
-    opacity: ${({ cartIsOpen }) => (cartIsOpen ? '0.7' : '1')};
-`
+  padding: 3rem 0;
+  background-color: #6e85b7;
+  opacity: ${({ cartIsOpen }) => (cartIsOpen ? "0.7" : "1")};
+  height: calc(100vh - 80px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
-export const LoginSection = styled.section`
+export const LoginSection = styled.section``;
 
-`
-
-export const LoginContainer = styled.div`
-
-`
+export const LoginContainer = styled.div``;
 
 export const FormBox = styled.div`
-    background-color: #fff;
-    border-radius: 10px;
-    max-width: 330px;
-    padding: 35px;
-    margin: auto;
+  background-color: #fff;
+  border-radius: 10px;
+  max-width: 330px;
+  padding: 35px;
+  margin: auto;
 
-    @media screen and (min-width: 768px) {
-        max-width: 420px;
-    }
-`
+  @media screen and (min-width: 768px) {
+    max-width: 420px;
+  }
+`;
 
-export const LoginForm = styled.form`
-
-`
+export const LoginForm = styled.form``;
 
 export const LoginTitle = styled.p`
-    font-size: 40px;
-    text-align: center;
-    padding-bottom: 1rem;
-    font-weight: 600;
-`
+  font-size: 40px;
+  text-align: center;
+  padding-bottom: 1rem;
+  font-weight: 600;
+`;
 
 export const FormUsername = styled.div`
-    margin-top: 1rem;
-`
+  margin-top: 1rem;
+`;
 
 export const Span = styled.span`
-    font-size: 14px;
-    font-weight: 600;
-`
+  font-size: 14px;
+  font-weight: 600;
+`;
 
 export const UserName = styled.div`
-    display: flex;
-    border-bottom: 2px solid gray;
-`
+  display: flex;
+  border-bottom: 2px solid gray;
+`;
 
 export const Input = styled.input`
-    width: 100%;
-    border: none;
-    cursor: pointer;
-    outline: none;
-    font-family: inherit;
-    font-size: 16px;
-`
+  width: 100%;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  font-family: inherit;
+  font-size: 16px;
+`;
 
 export const FormPassword = styled.div`
-    margin-top: 1rem;
-`
+  margin-top: 1rem;
+`;
 
 export const UserPassword = styled.div`
-    display: flex;
-    border-bottom: 2px solid gray;
-`
+  display: flex;
+  border-bottom: 2px solid gray;
+`;
 
 export const FormLink = styled.div`
-    margin: 0.5rem 0;
-    text-align: right;
-`
+  margin: 0.5rem 0;
+  text-align: right;
+`;
 
 export const ForgotPass = styled.a`
-    text-decoration: none;
-    font-size: 12px;
-`
+  text-decoration: none;
+  font-size: 12px;
+`;
 
 export const FormError = styled.div`
-    width: 100%;
-    line-height: 24px;
-    text-align: center;
-    margin: 1rem auto;
-    background: red;
-    border-radius: 5px;
-`
+  width: 100%;
+  line-height: 24px;
+  text-align: center;
+  margin: 1rem auto;
+  background: red;
+  border-radius: 5px;
+`;
 
 export const FormErrorP = styled.p`
-    color: white;
-`
+  color: white;
+`;
 
-export const FormBtn = styled.div`
-
-`
+export const FormBtn = styled.div``;
 
 export const FormButton = styled.button`
-    width: 100%;
-    height: 30px;
-    margin: 1rem 0;
-    border: none;
-    border-radius: 10px;
-    cursor: pointer;
-    background: black;
-    color: white;
+  width: 100%;
+  height: 30px;
+  margin: 1rem 0;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  background: black;
+  color: white;
 
-    &:hover {
-        scale: 1.1;
-    }
+  &:hover {
+    scale: 1.1;
+  }
 
-    &:disabled {
-        opacity: 0.8;
-        cursor: not-allowed;
-    }
-`
+  &:disabled {
+    opacity: 0.8;
+    cursor: not-allowed;
+  }
+`;
 
-export const FormText = styled.div`
-
-`
+export const FormText = styled.div``;
 
 export const FormTextP = styled.p`
-    font-size: 13px;
-    text-align: center;
-`
+  font-size: 13px;
+  text-align: center;
+`;
 
 export const FormIcons = styled.div`
-    display: flex;
-    justify-content: center;
-    gap: 1rem;
-    margin: 1rem;
-    font-size: 25px;
-`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin: 1rem;
+  font-size: 25px;
+`;
 
 export const IconBtn = styled.button`
-    border: none;
-    background: transparent;
-    cursor: pointer;
-`
+  border: none;
+  background: transparent;
+  cursor: pointer;
+`;
 
-export const Register = styled.div`
-
-`
+export const Register = styled.div``;
 
 export const RegisterLink = styled(Link)`
-    text-decoration: none;
-    color: inherit;
+  text-decoration: none;
+  color: inherit;
 
-    &:hover {
-        color: red;
-        text-decoration: underline;
-    }
-`
+  &:hover {
+    color: red;
+    text-decoration: underline;
+  }
+`;
--- Client/src/components/Login/Register/RegisterStyle.js
+++ Client/src/components/Login/Register/RegisterStyle.js
@@ -1,109 +1,113 @@
 import styled from "styled-components";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export const Main = styled.main`
-    padding: 3rem 0;
-    background-color: #6e85b7;
-    opacity: ${({ cartIsOpen }) => cartIsOpen ? "0.7" : "1"};
-`
+  padding: 3rem 0;
+  background-color: #6e85b7;
+  opacity: ${({ cartIsOpen }) => (cartIsOpen ? "0.7" : "1")};
+  height: calc(100vh - 80px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
-export const RegisterSection = styled.section``
+export const RegisterSection = styled.section``;
 
-export const RegisterContainer = styled.div``
+export const RegisterContainer = styled.div``;
 
 export const FormBox = styled.div`
-    background-color: #fff;
-    border-radius: 10px;
-    max-width: 330px;
-    padding: 30px;
-    margin: auto;
+  background-color: #fff;
+  border-radius: 10px;
+  max-width: 330px;
+  padding: 30px;
+  margin: auto;
 
-    @media screen and (min-width: 768px) {
-        max-width: 420px;
-    }
-`
+  @media screen and (min-width: 768px) {
+    max-width: 420px;
+  }
+`;
 
-export const RegisterForm = styled.form``
+export const RegisterForm = styled.form``;
 
 export const RegisterTitle = styled.p`
-    font-size: 40px;
-    text-align: center;
-    padding-bottom: 1rem;
-    font-weight: 600;
-`
+  font-size: 40px;
+  text-align: center;
+  padding-bottom: 1rem;
+  font-weight: 600;
+`;
 
 export const InputDataContainer = styled.div`
-    padding: 0 2rem;
-`
+  padding: 0 2rem;
+`;
 
 export const FormDataContainer = styled.div`
-    margin-top: 1.2rem;
-`
+  margin-top: 1.2rem;
+`;
 
 export const Span = styled.span`
-    font-size: 14px;
-    font-weight: 600;
-`
+  font-size: 14px;
+  font-weight: 600;
+`;
 
 export const Data = styled.div`
-    display: flex;
-    border-bottom: 2px solid gray;
-`
+  display: flex;
+  border-bottom: 2px solid gray;
+`;
 
 export const Input = styled.input`
-    width: 100%;
-    border: none;
-    cursor: pointer;
-    outline: none;
-    font-family: inherit;
-    font-size: 16px;
-`
+  width: 100%;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  font-family: inherit;
+  font-size: 16px;
+`;
 
 export const FormError = styled.div`
-    width: 100%;
-    /* height: 40px; */
-    line-height: 24px;
-    text-align: center;
-    margin: 1rem auto;
-    background: red;
-    border-radius: 5px;
-`
+  width: 100%;
+  /* height: 40px; */
+  line-height: 24px;
+  text-align: center;
+  margin: 1rem auto;
+  background: red;
+  border-radius: 5px;
+`;
 
 export const FormErrorP = styled.p`
-    /* background: red; */
-    color: white;
-`
+  /* background: red; */
+  color: white;
+`;
 
-export const FormBtn = styled.div``
+export const FormBtn = styled.div``;
 
 export const FormButton = styled.button`
-    width: 100%;
-    height: 30px;
-    margin: 1rem 0;
-    border: none;
-    border-radius: 10px;
-    cursor: pointer;
-    background: black;
-    color: white;
+  width: 100%;
+  height: 30px;
+  margin: 1rem 0;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  background: black;
+  color: white;
 
-    &:hover {
-        scale: 1.1;
-    }
-`
+  &:hover {
+    scale: 1.1;
+  }
+`;
 
 export const Return = styled.div`
-    margin: 1rem auto;
-`
+  margin: 1rem auto;
+`;
 
 export const ReturnLink = styled(Link)`
-    text-decoration: none;
-    color: inherit;
-    display: flex;
-    gap: 0.6rem;
-    padding: 0 2rem;
+  text-decoration: none;
+  color: inherit;
+  display: flex;
+  gap: 0.6rem;
+  padding: 0 2rem;
 
-    &:hover {
-        color: red;
-        text-decoration: underline;
-    }
-`
+  &:hover {
+    color: red;
+    text-decoration: underline;
+  }
+`;
